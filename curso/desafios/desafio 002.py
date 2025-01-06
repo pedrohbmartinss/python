d00b0904c848294c5e84f747c9812feaa874a435
Pedro Henrique Barbosa Martins <pedro.henriquecamargoo@gmail.com>
2025-01-06 13:42:05 -0300

    exe e desafios 1, 2 e 3

---
+++ curso/desafios/desafio 002.py
@@ -0,0 +1,4 @@
+dia = input('Qual o dia de seu nascimento?')
+mes = input('Qual o mes de seu nascimento?')
+ano = input('Qual o ano de nascimento?')
+print('Você nasceu no dia', dia, 'de ' + mes + ' de', ano,'. Correto?')
---
+++ curso/desafios/desafio 003.py
@@ -0,0 +1,4 @@
+primeiro = int(input('Digite o primeiro numero: '))
+segundo = int(input('Digite o segundo numero: '))
+soma = (primeiro + segundo)
+print('A soma entre {} e {} vale {}'.format(primeiro, segundo, soma))
