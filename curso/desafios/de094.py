3982423bba6a01e6849569b4fcab4de9bf9b3aff
Pedro Henrique Barbosa Martins <pedro.henriquecamargoo@gmail.com>
2025-02-13 13:56:37 -0300

    finalizado exercicios de dicionarios

--- curso/desafios/de094.py
+++ curso/desafios/de094.py
@@ -1,4 +1,31 @@
-pessoa = []
+pessoa = {}
 opcao = ''
-while opcao != 'N':
+lista = []
+cont = 0
+soma = media = 0
+while True:
+    pessoa.clear()
+    pessoa['nome'] = input('Nome: ')
+    while True:
+        pessoa['sexo'] = input('Sexo: ').upper()
+        if pessoa['sexo'] != 'M' and pessoa['sexo'] != 'F':
+            print('ERRO! Por favor, digite apenas M ou F')
+        else:
+            break
+    pessoa['idade'] = int(input('Idade: '))
+    soma += pessoa['idade']
+    lista.append(pessoa.copy())
+    opcao = input('Quer continuar? [S/N]').upper()
+    if opcao == 'N':
+        break
+print(lista)
+media = soma/len(lista)
+print(f'A) Ao todo temos {len(lista)} pessoas cadastradas.')
+print(f'B) A media de idade é {media:5.2f} anos')
+print(f'C) As mulheres cadastradas foram ', end='')
+for pessoa in lista:
+    if pessoa['sexo'] == 'F':
+        print(f'{pessoa['nome']}', end=' ')
+print()
+
     
