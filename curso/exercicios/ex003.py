a455465d7b59f0d66b936e40a8963eaab80f91c4
Pedro Henrique Barbosa Martins <pedro.henriquecamargoo@gmail.com>
2025-05-08 11:56:40 -0300

    ultima aula de python

--- curso/exercicios/ex003.py
+++
@@ -1,4 +0,0 @@
-primeiro = int(input('Digite o primeiro numero: '))
-segundo = int(input('Digite o segundo numero: '))
-soma = (primeiro + segundo)
-print('A soma entre {} e {} vale {}'.format(primeiro, segundo, soma))
