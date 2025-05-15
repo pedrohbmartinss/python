091b81b71494bead2ca1d38315c19c4017261ddb
Pedro Henrique Barbosa Martins <pedro.henriquecamargoo@gmail.com>
2025-05-15 10:24:45 -0300

    curso finalizado

--- curso/de115.py/menu.py
+++ curso/de115.py/menu.py
@@ -1,21 +1,29 @@
 import modulos
+arq = 'cursoemvideo.txt'
+
+if not modulos.arquivoexiste(arq):
+    modulos.criararquivo(arq)
 
 while True:
     modulos.menu()
     opcao = modulos.verificaint('Sua opção: ')
     if opcao == 1:
         print('-' * 40)
-        print('                  OPÇÃO 1')
+        print('                  PESSOAS CADASTRADAS')
         print('-' * 40)
+        modulos.lerarquivo(arq)
         continue
     elif opcao == 2:
         print('-' * 40)
-        print('                  OPÇÃO 2')
+        print('                  CADASTRAR PESSOA')
         print('-' * 40)
+        nome = str(input('NOME: '))
+        idade = int(input('IDADE: '))
+        modulos.cadastrar(arq, nome, idade)
         continue
     else:
         print('-' * 40)
         print('Saindo do sistema... Até logo!')
         print('-' * 40)
         break
-    
+ 
--- curso/de115.py/modulos.py
+++ curso/de115.py/modulos.py
@@ -19,3 +19,48 @@ def menu():
     print('1 - Ver pessoas cadastradas')
     print('2 - Cadastrar nova pessoa')
     print('3 - Sair do sistema')
+
+
+def arquivoexiste(nome):
+    try:
+        a = open(nome,'rt')
+        a.close()
+    except FileNotFoundError:
+        print('Arquivo não encontrado')
+        return False
+    else:
+        print('Arquivo encontrado')
+        return True
+
+def criararquivo(nome):
+    try:
+        a = open(nome, 'wt+')
+        a.close()
+    except:
+        print('Problema ao criar arquivo')
+    else:
+        print('Arquivo criado com sucesso')
+    
+
+def lerarquivo(nome):
+    try:
+        a = open(nome, 'rt')
+    except:
+        print('Erro ao ler arquivo')
+    else:
+        for linha in a:
+            dado = linha.split(';')
+            dado[1] = dado[1].replace('\n', '')
+            print(f'{dado[0]:<30}{dado[1]:>3} anos')
+    finally:
+        a.close()
+
+
+def cadastrar(arq, nome, idade):
+    try:
+        a = open(arq, 'at')
+    except:
+        print('Erro ao tentar cadastrar pessoa')
+    else:
+        a.write(f'{nome}; {idade}\n')
+        a.close()
